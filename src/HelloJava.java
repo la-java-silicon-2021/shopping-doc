1c29a0470b89c814531da4ccf7a90761d22859c8
清水 雅史 <47343094+shimizumasashi@users.noreply.github.com>
2021-06-02 13:56:54 +0900

    Delete HelloJava.java

--- src/HelloJava.java
+++
@@ -1,9 +0,0 @@
-public class HelloJava {
-	public static void main(String[] args) {
-		System.out.println("HelloJava!");
-		System.out.println("清水です");// 自己紹介！
-		System.out.println("Java講師です");
-		System.out.println("ブランチ切り替えました！");
-		System.out.println("001機能を追加");
-	}
-}
